6e731d986617ba100734f5dadfe02739874a14fe
AyushiAnand70047 <ayushianand088@gmail.com>
2025-02-28 22:21:15 +0530

    login page and signup page created

--- myproject/authentication/urls.py
+++ myproject/authentication/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login/', views.login),
+    path('signup/', views.signup),
 ]
