bc2c9fcdea98a692418381e3918405ea90af57ec
AyushiAnand70047 <ayushianand088@gmail.com>
2025-03-01 22:49:58 +0530

    update URL path for admin home and add user home CSS

--- myproject/organiser/urls.py
+++ myproject/organiser/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home,name='admin-home'),
+    path('admin-home/', views.home,name='admin-home'),
     path('live-stream/',views.livestream),
 ]
